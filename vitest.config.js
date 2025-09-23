ef9b1e043e8db179cfd3cd907b27c00807c9ff3d
RyomaY <61830629+ryoma-yama@users.noreply.github.com>
2025-09-23 02:47:58 +0000

    chore: add vitest

---
+++ vitest.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	test: {
+		environment: "node",
+		include: ["tests/**/*.test.js"],
+		pool: "threads",
+		poolOptions: {
+			threads: {
+				minThreads: 1,
+				maxThreads: 1,
+			},
+		},
+		coverage: {
+			provider: "v8",
+			all: true,
+			include: ["scripts/add_marker.js"],
+			reporter: ["text", "json-summary"],
+			thresholds: {
+				branches: 100,
+				functions: 100,
+				lines: 100,
+				statements: 100,
+			},
+		},
+	},
+});
